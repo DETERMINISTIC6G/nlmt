a39eadb4e21973ab4eb1a6655026a5d3071a1ebe
Samie Mostafavi <ssmos@kth.se>
2023-07-25 16:14:37 +0200

    update version

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package nlmt
 
 // Version is the IRTT version number (replaced during build).
-var Version = "0.9.7"
+var Version = "0.9.8"
 
 // ProtocolVersion is the protocol version number, which must match between client
 // and server.
